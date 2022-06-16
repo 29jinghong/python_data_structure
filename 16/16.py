cafff6bf290057127b629ae23f97b2f58a69bc28
jinghong wang <29jinghong@gmail.com>
2022-06-15 20:01:16 -0400

    fixing lession 16

--- 16/16.py
+++ 16/16.py
@@ -7,19 +7,23 @@ cur = conn.cursor()
 #you send the sqlite commands throw this cursor and get the respownd throw this cursor
 
 cur.execute('DROP TABLE IF EXISTS Counts')
-#this is a gardian line if there is aready a table created it will drop the table
+#this is a guardian line if there is aready a table created it will drop the table
 
 cur.execute('CREATE TABLE Counts (org TEXT, count INTEGER)')
-#creat the table to hold two viribles
+#creat the table to hold two viribles(two colums)
 
 fname = input('Enter file name: ')
-if (len(fname) < 1): fname = 'mbox-short.txt'
+if (len(fname) < 1): fname = 'mbox.txt'
 fh = open(fname)
+
 for line in fh:
     if not line.startswith('From: '): continue
     pieces = line.split()
-    org = pieces[1]
+    email = pieces[1].split('@')
+    org = email[1]
+
     cur.execute('SELECT count FROM Counts WHERE org = ? ', (org,))#tuple for email
+    #prepared statement google up
     #use the cursor to open the file
     row = cur.fetchone()
     #retrieves the next row of a query result set and returns a single sequence !!!
@@ -31,8 +35,10 @@ for line in fh:
         cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?', (org,))
         #the ? is a place holder for email
         #uses update because better for multyple connection
-    conn.commit()
-    #it writes every thing into disc
+
+conn.commit()
+#it writes every thing into disc
+#SQL transaction
 
 # https://www.sqlite.org/lang_select.html
 sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
@@ -41,4 +47,6 @@ for row in cur.execute(sqlstr):
     print(str(row[0]), row[1])
 #prints every thing
 cur.close()
+conn.close()
+
 #closes the curosur
